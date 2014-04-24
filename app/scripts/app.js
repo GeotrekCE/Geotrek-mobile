83a901c4a131f4f6c668eb52e2dd7154e9ff98dc
Natsu- <romain.garrigues.cs@gmail.com>
2014-04-24 14:43:31 +0200

    Initial AngularJS base commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,18 @@
 'use strict';
 
-angular
-  .module('geotrekMobileApp', []);
+var geotrekApp = angular.module('geotrekMobileApp', ['ngRoute', 'geotrekMobileControllers']);
+
+geotrekApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/', {
+          templateUrl : 'views/trek_list.html',
+            controller: 'TrekListController'
+        }).
+        when('/map', {
+          templateUrl : 'views/map.html',
+          controller: 'MapController'
+        }).
+        otherwise({
+            redirectTo : '/'
+        });
+}]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,9 @@
 'use strict';
 
-angular.module('geotrekMobileApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('geotrekMobileControllers', [])
+  .controller('TrekListController', function ($scope) {
+    $scope.description = 'Trek List !';
+  })
+  .controller('MapController', function ($scope) {
+    $scope.description = 'Global Map !';
   });
