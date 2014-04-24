0be7de93e758515ec5dc6154f4d555e81cced792
Adrien Grsmto <grsmto@gmail.com>
2014-04-24 18:54:04 +0200

    Display trek list with test geojson

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,16 +3,16 @@
 var geotrekApp = angular.module('geotrekMobileApp', ['ngRoute', 'geotrekMobileControllers']);
 
 geotrekApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-        when('/', {
-          templateUrl : 'views/trek_list.html',
-            controller: 'TrekListController'
-        }).
-        when('/map', {
-          templateUrl : 'views/map.html',
-          controller: 'MapController'
-        }).
-        otherwise({
-            redirectTo : '/'
-        });
+  $routeProvider.
+    when('/', {
+      templateUrl : 'views/trek_list.html',
+      controller: 'TrekListController'
+    }).
+    when('/map', {
+      templateUrl : 'views/map.html',
+      controller: 'MapController'
+    }).
+    otherwise({
+      redirectTo : '/'
+    });
 }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('geotrekMobileControllers', [])
-  .controller('TrekListController', function ($scope) {
+  .controller('TrekListController', function ($scope, $http) {
     $scope.description = 'Trek List !';
+
+    $http.get('trek.geojson').success(function(data) {
+      $scope.treks = data.features.splice(0, 10);
+    });
   })
   .controller('MapController', function ($scope) {
     $scope.description = 'Global Map !';
