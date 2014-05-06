334b58d9d4c26f81909c08b39d71644f63ef53f8
Adrien Grsmto <grsmto@gmail.com>
2014-05-06 14:26:59 +0200

    Refactor filters data to its specific service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 
 angular.module('geotrekMobileControllers', ['leaflet-directive'])
-.controller('TrekListController', function ($scope, TreksData) {
+.controller('TrekListController', function ($scope, TreksData, TreksFilters) {
     $scope.description = 'Trek List !';
 
     TreksData.getTreks().then(function(treks) {
@@ -14,95 +14,55 @@ angular.module('geotrekMobileControllers', ['leaflet-directive'])
 
 
     // Filters
-    $scope.difficulties = [
-        { value: '1', name: 'Facile' },
-        { value: '2', name: 'Moyen' },
-        { value: '3', name: 'Difficile' }
-    ];
-
-    $scope.durations = [
-        { value: '4', name: '2H30' },
-        { value: '10', name: '1/2' },
-        { value: '10.1', name: 'Journée' }
-    ];
-
-    $scope.elevations = [
-        { value: '300', name: '300m' },
-        { value: '600', name: '600m' },
-        { value: '1000', name: '1000m' }
-    ];
+    $scope.difficulties = TreksFilters.difficulties;
+    $scope.durations    = TreksFilters.durations;
+    $scope.elevations   = TreksFilters.elevations;
 
     $scope.activeFilters = {
         difficulty: undefined,
         duration: undefined,
         elevation: undefined
-    }
+    };
 
     $scope.filterTreks = function (trek) {
-        console.log(trek);
         if (filterTrekWithFilter(trek.properties.difficulty.id, $scope.difficulties, 'difficulty') &&
             filterTrekWithFilter(trek.properties.duration, $scope.durations, 'duration') &&
-            filterTrekWithFilter(trek.properties.elevation, $scope.elevations, 'elevation'))
-             return true;
+            filterTrekWithFilter(trek.properties.ascent, $scope.elevations, 'elevation')) {
+            return true;
+        }
         return false;
     };
 
     function filterTrekWithFilter(trekValue, category, property) {
-        var value = trekValue;
         // console.log(trekValue);
+        // console.log($scope.activeFilters[property]);
+        
         // Trek considered as matching if filter not set or if
         // property is empty.
-        if (value === undefined ||
+        if (trekValue === undefined ||
             angular.isUndefined($scope.activeFilters[property]) ||
             $scope.activeFilters[property] === null) {
             return true;
         }
 
-        var rangeValues = self._values[category],
-            rangeMin = 0,
-            rangeMax = rangeValues.length-1,
-            min = self.state.sliders[category].min,
-            max = self.state.sliders[category].max;
-
-        if (max === rangeMin) {
-            // Both on minimum value
-            return value <= rangeValues[rangeMin];
+        if (trekValue <= $scope.activeFilters[property]) {
+            return true;
+        } else {
+            return false;
         }
-        if (min === rangeMax) {
-            // Both on maximum values
-            return value >= rangeValues[rangeMax];
-        }
-
-        var minVal = rangeValues[min - 1],
-            maxVal = rangeValues[max + 1];
-
-        if (category == 'altitude' && min != max) {
-            minVal = rangeValues[min];
-            maxVal = rangeValues[max];
-        }
-
-        if (min === rangeMin) {
-            // Filter by max only
-            return value < maxVal;
-        }
-        if (max === rangeMax) {
-            // Filter by min only
-            return value > minVal;
-        }
-        return value > minVal && value < maxVal;
     }
 
     $scope.durationFilter = function (trek) {
         if (angular.isUndefined($scope.activeFilters.duration) || $scope.activeFilters.duration === null) {
             return true;
-        };
+        }
 
         if (trek.properties.duration <= $scope.activeFilters.duration) {
             return true;
         } else {
             return false;
         }
-    }
+    };
 })
 .controller('TrekController', function ($scope, $routeParams, TreksData, $sce) {
     $scope.description = 'Trek detail !';
