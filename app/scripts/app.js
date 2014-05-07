8d888e878a93637a8b57f4f7aca32a6c1f2a9969
Adrien Grsmto <grsmto@gmail.com>
2014-05-07 15:58:52 +0200

    Switch app to angular-ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,36 @@
 'use strict';
 
-var geotrekApp = angular.module('geotrekMobileApp', ['ngRoute', 'ngResource', 'ui.bootstrap.buttons', 'geotrekMobileControllers', 'geotrekMobileServices']);
+var geotrekApp = angular.module('geotrekMobileApp', ['ngResource', 'ui.router', 'ui.bootstrap.buttons', 'geotrekMobileControllers', 'geotrekMobileServices']);
 
-geotrekApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-    when('/', {
-        redirectTo: '/trek'
-    }).
-    when('/trek', {
+geotrekApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/trek');
+
+    $stateProvider
+    .state('trek', {
+        url: '/trek',
+        abstract: true,
+        template: '<div ui-view></div>'
+    })
+    .state('trek.list', {
+        url: '',
         templateUrl : 'views/trek_list.html',
         controller: 'TrekListController'
-    }).
-    when('/trek/:trekId', {
+    })
+    .state('trek.detail', {
+        url:'/:trekId',
         templateUrl : 'views/trek_detail.html',
         controller: 'TrekController'
-    }).
-    when('/map', {
+    })
+    .state('map', {
+        url:'/map',
         templateUrl : 'views/global_map.html',
         controller: 'MapController'
-    }).
-    when('/map/:trekId', {
+    })
+    .state('map.trek', {
+        url:'/:trekId',
         templateUrl : 'views/global_map.html',
         controller: 'MapController'
-    }).
-    otherwise({
-        redirectTo : '/'
     });
 
-}]);
+});
