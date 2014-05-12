ed61d9eac67018ac883a7aa1823ef3b50174b7b1
Adrien Grsmto <grsmto@gmail.com>
2014-05-12 17:01:08 +0200

    New service to read/write data with Cordova API + desktop mock

--- app/scripts/services/treks-data.js
+++ app/scripts/services/treks-data.js
@@ -5,7 +5,7 @@ var services = angular.module('geotrekMobileServices', ['ngResource']);
 /**
  * Service that persists and retrieves treks from data source
  */
-services.factory('TreksData', function ($resource, $rootScope, $window, $q) {
+services.factory('TreksData', function ($resource, $rootScope, $window, $q, Files) {
     var CACHED_FILE = 'trek.geojson',
         REMOTE_FILE = 'http://rando.makina-corpus.net/fr/filesapi/trek/trek.geojson';
 
@@ -46,105 +46,41 @@ services.factory('TreksData', function ($resource, $rootScope, $window, $q) {
             }
 
             function getLocalFile() {
-                // If currently on Cordova
-                if (angular.isDefined($window.cordova)) {
-                    console.log('cordova');
-                    var _deferred = $q.defer(); 
-                    
-                    // Cordova shit must return a file from SDCard
-                    $window.requestFileSystem($window.PERSISTENT, 5*1024*1024 /*5MB*/, function(filesystem) {
-                        console.log('requestFileSystem');
-                        filesystem.root.getFile(CACHED_FILE, {create: false}, 
-                            function(fileEntry) {
-                                _deferred.resolve(fileEntry.file);
-                            },
-                            function() {
-                                _deferred.reject('no file found');
-                            }
-                        );
-                    }, errorHandler);
+                console.log('getLocalFile');
 
-                    return _deferred.promise;
-                } else {
-                    // Else, we're on desktop or anywhere else, download it from the build
-                    _deferred = requests.query({ url: CACHED_FILE, method: 'GET' }).$promise;
-
-                    return _deferred;
-                }
-            }
-
-            function errorHandler(e) {
-              var msg = '';
-
-              switch (e.code) {
-                case FileError.QUOTA_EXCEEDED_ERR:
-                  msg = 'QUOTA_EXCEEDED_ERR';
-                  break;
-                case FileError.NOT_FOUND_ERR:
-                  msg = 'NOT_FOUND_ERR';
-                  break;
-                case FileError.SECURITY_ERR:
-                  msg = 'SECURITY_ERR';
-                  break;
-                case FileError.INVALID_MODIFICATION_ERR:
-                  msg = 'INVALID_MODIFICATION_ERR';
-                  break;
-                case FileError.INVALID_STATE_ERR:
-                  msg = 'INVALID_STATE_ERR';
-                  break;
-                default:
-                  msg = 'Unknown Error';
-                  break;
-              };
-
-              console.log('Error: ' + msg);
+                return Files.read(CACHED_FILE);
             }
 
             // First, check if we have a local file
             getLocalFile()
-                .then(function(file) { // Local file found!
-                    console.log(file);
+                .then(function(results) { // Local file found!
+                    var file,
+                        meta;
+                    console.log('local file found!');
                     // Check if it's the last updated version
                     // checkLastVersion(file);
 
+                    file = results[0];
+                    meta = results[1];
+
                     var data = angular.fromJson(file);
                     deferred.resolve(data.features.splice(0, 10));
-                })
-                .catch(function(error) { // No local file found
+                },
+                function(error) { // No local file found
                     console.log(error);
 
                     getServerFile().$promise
                         .then(function(file) {
                             var data = angular.fromJson(file);
-                            
+
                             // Save it to local
-                            $window.requestFileSystem($window.PERSISTENT, 5*1024*1024 /*5MB*/, function(filesystem) {
-                                console.log('requestFileSystem');
-                                filesystem.root.getFile(CACHED_FILE, {create: true}, 
-                                    function(fileEntry) {
-                                        // Create a FileWriter object for our FileEntry (log.txt).
-                                        fileEntry.createWriter(function(fileWriter) {
-
-                                            fileWriter.onwriteend = function(e) {
-                                              console.log('Write completed.');
-                                            };
-
-                                            fileWriter.onerror = function(e) {
-                                              console.log('Write failed: ' + e.toString());
-                                            };
-
-                                            // Create a new Blob and write it to log.txt.
-                                            var blob = new Blob(data, {type: 'text/json'});
-
-                                            fileWriter.write(blob);
-
-                                        }, errorHandler);
-                                    },
-                                    function() {
-                                        _deferred.reject('no file found');
-                                    }
-                                );
-                            }, errorHandler);
+                            Files.save(CACHED_FILE, angular.toJson(file)).then(function(file) {
+                                console.log('saved successfully :');
+                                console.log(file);
+                            }, function(error) {
+                                console.log('save fail :');
+                                console.log(error);
+                            });
 
                             deferred.resolve(data.features.splice(0, 10));
                         }).catch(function(error) {
@@ -199,17 +135,99 @@ services.value('TreksFilters', {
 
 
 
-services.service('CordovaService', function($window, $document, $q, $rootScope) {
-    var d = $q.defer();
+/**
+ * Cordova File API Service
+ */
 
-    this.ready = d.promise;
-
-    // If currently on Cordova
-    if (angular.isDefined($window.cordova)) {
-        $document.addEventListener('deviceready', function() {
-            d.resolve();
+services.service('WebFiles', function ($window, $q, $resource) {
+    var requests = $resource(':url', {}, {
+            query: {
+                method: 'GET',
+                cache: true
+            }
         });
+
+    this.save = function(filename, content) {
+        var defered = $q.defer();
+        defered.resolve(true);
+        return defered.promise;
+    };
+
+    this.read = function(filename) {
+        return $q.all([requests.query({ url: filename, method: 'GET' }).$promise, "2014-05-12T13:51:58.000Z"]);
+    };
+
+    this.remove = function(filename) {
+        var defered = $q.defer();
+        defered.resolve(true);
+        return defered.promise;
+    };
+});
+
+services.service('CordovaFiles', function ($window, $q) {
+    var fstype = $window.PERSISTENT;
+    var path_prefix = 'geotrek/';
+
+    $window.requestFileSystem(fstype, 0, function gotFS(fileSystem) {
+        var dataDir = fileSystem.root.getDirectory(path_prefix, {create: true});
+    }, function() {
+        throw 'cannot-create-root-directory';
+    });
+
+    this.save = function(filename, content) {
+        var defered = $q.defer();
+        window.requestFileSystem(fstype, 0, function gotFS(fileSystem) {
+            fileSystem.root.getFile(
+                path_prefix + filename,
+                {create: true, exclusive: false},
+                function gotFileEntry(fileEntry) {
+                    fileEntry.createWriter(function gotFileWriter(writer) {
+                        writer.onwriteend = defered.resolve;
+                        writer.write(content);
+                    }, defered.reject);
+                }, defered.reject);
+        }, defered.reject);
+        return defered.promise;
+    };
+
+    this.read = function(filename) {
+        var deferedFile = $q.defer(),
+            deferedMeta = $q.defer();
+        $window.requestFileSystem(fstype, 0, function gotFS(fileSystem) {
+            fileSystem.root.getFile(path_prefix + filename,
+                null, function gotFileEntry(fileEntry) {
+                fileEntry.file(function gotFile(file){
+                    var reader = new FileReader();
+                    reader.onloadend = function(evt) {
+                        deferedFile.resolve(evt.target.result);
+                    };
+                    reader.readAsText(file);
+                }, deferedFile.reject);
+
+                fileEntry.getMetadata(function gotMetadata(metadata) {
+                    deferedMeta.resolve(metadata);
+                }, deferedMeta.reject);
+            }, deferedFile.reject);
+        }, deferedFile.reject);
+        return $q.all([deferedFile.promise, deferedMeta.promise]);
+    };
+
+    this.remove = function(filename) {
+        var defered = $q.defer();
+        $window.requestFileSystem(fstype, 0, function gotFS(fileSystem) {
+            fileSystem.root.getFile(path_prefix + filename,
+                null, function gotFileEntry(fileEntry) {
+                fileEntry.remove(defered.resolve, defered.reject);
+            }, defered.reject);
+        }, defered.reject);
+        return defered.promise;
+    };
+});
+
+services.factory('Files', function($window, $injector) {
+    if ($window.cordova) {
+        return $injector.get('CordovaFiles');
     } else {
-        d.resolve();
+        return $injector.get('WebFiles');
     }
 });
