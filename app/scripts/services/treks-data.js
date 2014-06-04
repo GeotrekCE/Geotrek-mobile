282395155f2a405a4b4e676fefa20a3ccf828c0c
Adrien Grsmto <grsmto@gmail.com>
2014-06-04 17:52:48 +0200

    filters 4 rows

--- app/scripts/services/treks-data.js
+++ app/scripts/services/treks-data.js
@@ -117,23 +117,24 @@ services.factory('TreksData', function ($resource, $rootScope, $window, $q, File
 
 services.value('TreksFilters', {
     difficulties : [
-        { value: 1, name: 'Facile', icon: 'icon-geotrek-difficulty1' },
-        { value: 2, name: 'Moyen', icon: 'icon-geotrek-difficulty2' },
-        { value: 3, name: 'Difficile', icon: 'icon-geotrek-difficulty2' }
+        { value: 1, name: 'Facile', icon: 'difficulty-1.svg' },
+        { value: 2, name: 'Moyen', icon: 'difficulty-2.svg' },
+        { value: 3, name: 'Difficile', icon: 'difficulty-2.svg' },
+        { value: 4, name: 'Difficile', icon: 'difficulty-2.svg' }
     ],
     durations : [
-        { value: 2.5, name: '<2H30', icon: 'icon-geotrek-difficulty2' },
-        { value: 4, name: '1/2', icon: 'icon-geotrek-difficulty2' },
-        { value: 8, name: 'Journée', icon: 'icon-geotrek-difficulty2' }
+        { value: 2.5, name: '<2H30', icon: 'duration-1.svg' },
+        { value: 4, name: '1/2', icon: 'duration-2.svg' },
+        { value: 8, name: 'Journée', icon: 'duration-3.svg' }
     ],
     elevations : [
-        { value: 300, name: '300m', icon: 'icon-geotrek-level1' },
-        { value: 600, name: '600m', icon: 'icon-geotrek-level1' },
-        { value: 1000, name: '1000m', icon: 'icon-geotrek-level1' }
+        { value: 300, name: '300m', icon: 'deniv1.svg' },
+        { value: 600, name: '600m', icon: 'deniv1.svg' },
+        { value: 1000, name: '1000m', icon: 'deniv1.svg' }
     ],
     themes : [
-        { value: 'walk', name: 'Marche', icon: 'icon-geotrek-level1' },
-        { value: 'walk', name: 'Marche', icon: 'icon-geotrek-level1' },
+        { value: 'walk', name: 'Marche', icon: 'deniv1.svg' },
+        { value: 'walk', name: 'Marche', icon: 'deniv1.svg' },
     ],
     communes : [
         { value: 'Pompertuzat', name: 'Pompertuzat' },
