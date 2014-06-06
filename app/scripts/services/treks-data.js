3006ada6eb2e08107375cb1a38a88534ba9e6b16
Adrien Grsmto <grsmto@gmail.com>
2014-06-06 16:48:05 +0200

    Fix a bug where modal view was loading slowly on mobile devices (do not rely on cache anymore)

--- app/scripts/services/treks-data.js
+++ app/scripts/services/treks-data.js
@@ -95,16 +95,30 @@ services.factory('TreksData', function ($resource, $rootScope, $window, $q, File
             var trekId = parseInt(_trekId);
             var trek;
 
-            return this.getTreks().then(function(treks) {
-                angular.forEach(treks.features, function(_trek) {
+            if (angular.isDefined($rootScope.treks)) {
+                var deferred = $q.defer();
+
+                angular.forEach($rootScope.treks.features, function(_trek) {
                     if (_trek.id === trekId) {
                         trek = _trek;
                         return;
                     }
                 });
 
-                return trek;
-            });
+                deferred.resolve(trek);
+                return deferred.promise;
+            } else {
+                return this.getTreks().then(function(treks) {
+                    angular.forEach(treks.features, function(_trek) {
+                        if (_trek.id === trekId) {
+                            trek = _trek;
+                            return;
+                        }
+                    });
+
+                    return trek;
+                });
+            }
         }
     };
 });
