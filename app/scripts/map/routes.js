6dc36818cbf6225def1dfb5b19ee77cff020b09c
Natsu- <romain.garrigues.cs@gmail.com>
2014-06-17 14:55:05 +0200

    Moving map controllers to map folder

--- app/scripts/map/routes.js
+++ app/scripts/map/routes.js
@@ -16,4 +16,51 @@ geotrekApp.config(function($stateProvider, $urlRouterProvider) {
         url: '/:trekId',
         controller: 'MapControllerDetail'
     });
+})
+.controller('MapController', function ($scope, leafletData, filterFilter) {
+    // Set default Leaflet map params
+    angular.extend($scope, {
+        center: {
+            lat: 44.8,
+            lng: 6.2,
+            zoom: 9
+        },
+        defaults: {
+            scrollWheelZoom: false,
+            zoomControl: false // Not needed on Android/iOS modern devices
+        }
+    });
+
+    // Add treks geojson to the map
+    function showTreks() {
+        angular.extend($scope, {
+            geojson: {
+                data: filterFilter($scope.treks.features, $scope.activeFilters.search),
+                filter: $scope.filterTreks,
+                style: {
+                    fillColor: 'green',
+                    weight: 2,
+                    opacity: 1,
+                    color: 'black',
+                    dashArray: '3',
+                    fillOpacity: 0.7
+                }
+            }
+        });
+    }
+
+    if (angular.isDefined($scope.treks)) { // If treks data are already loaded
+        showTreks();
+    } else { // Data not yet loaded, wait for loading, then display treks on map
+        $scope.$on('OnTreksLoaded', showTreks);
+    }
+
+    $scope.$on('OnFilter', function() {
+        if (angular.isDefined($scope.treks)) {
+            showTreks();
+        }
+    });
+})
+.controller('MapControllerDetail', function ($scope, $stateParams) {
+    console.log($stateParams);
 });
