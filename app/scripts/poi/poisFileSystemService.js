78cccb6f52a7f73581cf23a25515b8736628f14c
Natsu- <romain.garrigues.cs@gmail.com>
2014-06-20 17:35:27 +0200

    Issue #17: downloading and serving poi thumbnail and pictures in offline mode

--- app/scripts/poi/poisFileSystemService.js
+++ app/scripts/poi/poisFileSystemService.js
@@ -4,10 +4,14 @@ var geotrekPois = angular.module('geotrekPois');
 
 geotrekPois.service('poisFileSystemService', function ($resource, $rootScope, $window, $q, $cordovaFile) {
 
-    var REMOTE_FILE_URL = 'http://rando.makina-corpus.net/fr/files/api/trek',
+    var DOMAIN_NAME = 'http://rando.makina-corpus.net',
+        REMOTE_FILE_URL = DOMAIN_NAME + '/fr/files/api/trek',
         POI_FILE_NAME = 'pois.geojson',
         CDV_ROOT = 'cdvfile://localhost/persistent',
+        GEOTREK_DIR = 'geotrek',
         DIR_NAME = 'geotrek/trek',
+        POI_DIR_NAME = 'geotrek/poi',
+        POI_SUBDIR = 'poi',
         POI_FILENAME = 'pois.geojson';
 
     this._getPoisTrekAbsoluteURL = function(trekId) {
@@ -18,9 +22,32 @@ geotrekPois.service('poisFileSystemService', function ($resource, $rootScope, $w
         return DIR_NAME + '/' + trekId.toString() + '/' + POI_FILENAME;
     };
 
+    this.replaceImgURLs = function(poiData) {
+        var copy = angular.copy(poiData, {});
+
+        // Parse poi pictures, and change their URL
+        angular.forEach(copy.features, function(poi) {
+            var currentPoiId = poi.id;
+
+            var thumbnailUrl = poi.properties.thumbnail,
+                thFilename = thumbnailUrl.substr(thumbnailUrl.lastIndexOf('/') + 1);
+
+            poi.properties.thumbnail = CDV_ROOT + '/' + GEOTREK_DIR + '/' + POI_SUBDIR + '/' + currentPoiId.toString() + '/' + thFilename;
+
+            angular.forEach(poi.properties.pictures, function(picture) {
+                var pictureUrl = picture.url;
+                var filename = pictureUrl.substr(pictureUrl.lastIndexOf('/') + 1);
+
+                picture.url = CDV_ROOT + '/' + GEOTREK_DIR + '/' + POI_SUBDIR + '/' + currentPoiId.toString() + '/' + filename;
+            });
+        });
+        return copy;
+    };
+
     this.downloadPoisFromTrek = function(trekId) {
         var trekPoisURL = REMOTE_FILE_URL + '/' + trekId + '/' + POI_FILE_NAME,
-            trekPoisFilepath = this._getPoisTrekAbsoluteURL(trekId);
+            trekPoisFilepath = this._getPoisTrekAbsoluteURL(trekId),
+            _this = this;
 
         return this.hasPoisFromTrek(trekId)
         .then(function() {
@@ -31,6 +58,9 @@ geotrekPois.service('poisFileSystemService', function ($resource, $rootScope, $w
             // If not, let's go !!
             console.log('downloading ' + trekPoisURL + ' to ' + trekPoisFilepath);
             return $cordovaFile.downloadFile(trekPoisURL, trekPoisFilepath);
+        })
+        .then(function() {
+            return _this.downloadPoisImages(trekId);
         });
     };
 
@@ -46,25 +76,84 @@ geotrekPois.service('poisFileSystemService', function ($resource, $rootScope, $w
         return $q.all(promises);
     };
 
+    this.downloadPoisImages = function(trekId) {
+        var _this = this;
+
+        return this.getRawPoisFromTrek(trekId)
+        .then(function(pois) {
+            var promises = [];
+
+            angular.forEach(pois.features, function(poi) {
+                var currentPoiId = poi.id;
+
+                var thumbnailUrl = poi.properties.thumbnail;
+                if (!!thumbnailUrl) {
+                    filename = thumbnailUrl.substr(thumbnailUrl.lastIndexOf('/') + 1);
+                    promises.push(_this.downloadPoiImage(currentPoiId, filename, DOMAIN_NAME + thumbnailUrl))
+                }
+
+                angular.forEach(poi.properties.pictures, function(picture) {
+                    var pictureUrl = picture.url;
+                    var picFilename = pictureUrl.substr(pictureUrl.lastIndexOf('/') + 1);
+                    promises.push(_this.downloadPoiImage(currentPoiId, picFilename, DOMAIN_NAME + pictureUrl));
+                });
+            })
+
+            return $q.all(promises);
+        });
+    };
+
+    this.downloadPoiImage = function(poiId, pictureName, url) {
+        return $cordovaFile.checkFile(POI_DIR_NAME + '/' + poiId + '/' + pictureName)
+        .then(function() {
+            var deferred = $q.defer();
+            deferred.resolve({message: 'picture ' + pictureName + ' already present'});
+            return deferred.promise;
+            // picture already present, not downloading it
+        }, function() {
+            var path = CDV_ROOT + '/' + GEOTREK_DIR + '/' + POI_SUBDIR + '/' + poiId.toString() + '/' + pictureName;
+            console.log('downloading ' + url + ' to ' + path);
+            return $cordovaFile.downloadFile(url, path);
+        });
+    };
+
     this.hasPoisFromTrek = function(trekId) {
         var trekPoisFilepath = this._getPoisTrekRelativeURL(trekId);
         return $cordovaFile.checkFile(trekPoisFilepath);
     }
 
+    // Getting Pois used for mobile purpose
+    // Image urls are converted to cdv://localhost/persistent/... ones
     this.getPoisFromTrek = function(trekId) {
+        var replaceUrls = true;
+        return this._getPoisFromTrek(trekId, replaceUrls);
+    };
 
-        var trekPoisFilepath = this._getPoisTrekRelativeURL(trekId);
+    // Getting server trek original data
+    this.getRawPoisFromTrek = function(trekId) {
+        var replaceUrls = false;
+        return this._getPoisFromTrek(trekId, replaceUrls);
+    };
+
+    this._getPoisFromTrek = function(trekId, replaceUrls) {
+
+        var trekPoisFilepath = this._getPoisTrekRelativeURL(trekId),
+            deferred = $q.defer(),
+            _this = this;
 
         $cordovaFile.readFile(trekPoisFilepath)
         .then(
             function(data) {
                 var jsonData = JSON.parse(data);
-                defered.resolve(jsonData);
+                if (replaceUrls) {
+                    jsonData = _this.replaceImgURLs(jsonData);
+                }
+                deferred.resolve(jsonData);
             },
-            defered.reject
+            deferred.reject
         );
 
-        return defered.promise;
+        return deferred.promise;
     };
 
 });
