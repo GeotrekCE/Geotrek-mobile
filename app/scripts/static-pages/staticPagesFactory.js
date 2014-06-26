d3360f6975943a97a31f9c5cfae350c701e9b43d
Natsu- <romain.garrigues.cs@gmail.com>
2014-06-26 14:49:09 +0200

    Issue #2: static pages are loading from independant template file with html content

--- app/scripts/static-pages/staticPagesFactory.js
+++ app/scripts/static-pages/staticPagesFactory.js
@@ -9,12 +9,16 @@ var geotrekApp = angular.module('geotrekMobileApp');
 geotrekApp.factory('staticPagesFactory', function ($resource, $rootScope, $window, $q) {
     return {
         getStaticPages: function() {
-            var deferred = $q.defer();
+            var deferred = $q.defer(),
+                fakeText = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><p>Aliquam ac egestas est. Nunc ac nisl fringilla, tristique nibh quis, fermentum dui.</p><p>Nulla sit amet sodales mauris. Praesent tristique commodo tempus. Suspendisse vel blandit mi. In neque sapien, consequat eu gravida eu, interdum nec quam. Nullam eget suscipit mi. Morbi vestibulum nisl ut nisi lacinia, ut consectetur sapien ultricies.</p>';
+
+            // text field is used by ionic $ionicActionSheet.show() method to display menu titles
+            // title and description are used in static pages template
             var pages = [
-                { text: 'Page statique A' },
-                { text: 'Page statique B' },
-                { text: 'Page statique C' },
-                { text: 'Page statique D' }
+                { text: 'Page statique A', title: 'Page statique A', description: fakeText },
+                { text: 'Page statique B', title: 'Page statique B', description: fakeText },
+                { text: 'Page statique C', title: 'Page statique C', description: fakeText },
+                { text: 'Page statique D', title: 'Page statique D', description: fakeText }
             ];
 
             deferred.resolve(pages);
