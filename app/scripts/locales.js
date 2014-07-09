e99d1c31ffa7b217a9b2e0d02d40b988cb2e0e94
Natsu- <romain.garrigues.cs@gmail.com>
2014-07-09 11:50:52 +0200

    Issue #26: translating raw text in each app template

--- app/scripts/locales.js
+++ app/scripts/locales.js
@@ -7,12 +7,30 @@ geotrekLocales.constant('locales', {
     'fr': {
         'nav_trek_map.ways': 'Itinéraires',
         'nav_trek_map.map': 'Carte',
-        'nav_trek_map.cancel': 'Annuler'
+        'nav_trek_map.cancel': 'Annuler',
+        'nav_trek_map.city': 'Commune',
+        'nav_trek_map.reset': 'Réinitialiser',
+        'static_page.cancel': 'Annuler',
+        'static_page.back': 'Retour',
+        'trek_detail.description': 'Description',
+        'trek_detail.cities': 'Communes',
+        'trek_detail.on_road': 'En chemin',
+        'trek_list.departure': 'Départ',
+        'trek_list.distance': 'Distance'
     },
     'en': {
         'nav_trek_map.ways': 'Ways',
         'nav_trek_map.map': 'Map',
-        'nav_trek_map.cancel': 'Cancel'            
+        'nav_trek_map.cancel': 'Cancel',
+        'nav_trek_map.city': 'City',
+        'nav_trek_map.reset': 'Reset',
+        'static_page.cancel': 'Cancel',
+        'static_page.back': 'Back',
+        'trek_detail.description': 'Description',
+        'trek_detail.cities': 'Cities',
+        'trek_detail.on_road': 'On road',
+        'trek_list.departure': 'Departure',
+        'trek_list.distance': 'Distance'        
     }
 });
 
