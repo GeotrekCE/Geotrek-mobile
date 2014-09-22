246f61b2aa9f329254807b4dd15a234774eabfc2
Frederic Bonifas <fredericbonifas@gmail.com>
2014-09-22 11:53:31 +0200

    decode urls as we receive them encoded

--- app/scripts/trek/treksFileSystemService.js
+++ app/scripts/trek/treksFileSystemService.js
@@ -58,25 +58,25 @@ geotrekTreks.service('treksFileSystemService', function ($resource, $rootScope, 
             angular.forEach(treks.features, function(trek) {
                 var currentTrekId = trek.id;
                 angular.forEach(trek.properties.pictures, function(picture) {
-                    var pictureUrl = picture.url;
+                    var pictureUrl = decodeURIComponent(picture.url);
                     var serverUrl = settings.DOMAIN_NAME + pictureUrl;
                     var filename = pictureUrl.substr(pictureUrl.lastIndexOf('/') + 1);
                     promises.push(utils.downloadFile(serverUrl, _this.getTrekSubdir(currentTrekId) + '/' + filename));
                 });
                 angular.forEach(trek.properties.usages, function(usage) {
-                    var usageUrl = usage.pictogram;
+                    var usageUrl = decodeURIComponent(usage.pictogram);
                     var serverUrl = settings.DOMAIN_NAME + usageUrl;
                     var filename = usageUrl.substr(usageUrl.lastIndexOf('/') + 1);
                     promises.push(utils.downloadFile(serverUrl, _this.getTrekSubdir(currentTrekId) + '/' + filename));
                 });
                 angular.forEach(trek.properties.themes, function(theme) {
-                    var themeUrl = theme.pictogram;
+                    var themeUrl = decodeURIComponent(theme.pictogram);
                     var serverUrl = settings.DOMAIN_NAME + themeUrl;
                     var filename = themeUrl.substr(themeUrl.lastIndexOf('/') + 1);
                     promises.push(utils.downloadFile(serverUrl, _this.getTrekSubdir(currentTrekId) + '/' + filename));
                 });
                 angular.forEach(trek.properties.networks, function(network) {
-                    var networkUrl = network.pictogram;
+                    var networkUrl = decodeURIComponent(network.pictogram);
                     var serverUrl = settings.DOMAIN_NAME + networkUrl;
                     var filename = networkUrl.substr(networkUrl.lastIndexOf('/') + 1);
                     promises.push(utils.downloadFile(serverUrl, _this.getTrekSubdir(currentTrekId) + '/' + filename));
