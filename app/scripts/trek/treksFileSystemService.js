420dffd3368008ebe5d2d1bffaa738f4bef243c9
Julien Marinescu <jma@makina-corpus.com>
2014-09-22 09:15:12 +0200

    #70 manage/save difficulty pictogram on mobile

--- app/scripts/trek/treksFileSystemService.js
+++ app/scripts/trek/treksFileSystemService.js
@@ -34,6 +34,11 @@ geotrekTreks.service('treksFileSystemService', function ($resource, $rootScope, 
                 var filename = networkUrl.substr(networkUrl.lastIndexOf('/') + 1);
                 network.pictogram = settings.device.CDV_TREK_ROOT + '/' + currentTrekId.toString() + '/' + filename;
             });
+            if(angular.isDefined(trek.properties.difficulty)){
+                var difficultyUrl = trek.properties.difficulty.pictogram;
+                var filename = difficultyUrl.substr(difficultyUrl.lastIndexOf('/') + 1);
+                trek.properties.difficulty.pictogram = settings.device.CDV_PICTO_ROOT + '/' + filename;
+            }
         });
         return copy;
     };
@@ -71,6 +76,11 @@ geotrekTreks.service('treksFileSystemService', function ($resource, $rootScope, 
                     var filename = usageUrl.substr(usageUrl.lastIndexOf('/') + 1);
                     promises.push(utils.downloadFile(serverUrl, _this.getTrekSubdir(currentTrekId) + '/' + filename));
                 });
+                if(angular.isDefined(trek.properties.difficulty)){
+                    var difficultyUrl = trek.properties.difficulty.pictogram;
+                    var filename = difficultyUrl.substr(difficultyUrl.lastIndexOf('/') + 1);
+                    promises.push(utils.downloadFile(settings.DOMAIN_NAME + difficultyUrl, settings.device.CDV_PICTO_ROOT + '/' + filename));
+                }
             })
 
             return $q.all(promises);
