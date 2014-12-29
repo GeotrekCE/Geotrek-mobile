26d9558a373902e7603cd4b3d9dd79b5ec4791b5
Frederic Bonifas <fredericbonifas@gmail.com>
2014-12-29 10:50:43 +0100

    change map attribution

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -29,7 +29,7 @@ geotrekAppSettings.constant('globalSettings', {
         GLOBAL_MAP_DEFAULT_ZOOM: 12,
         GLOBAL_MAP_DEFAULT_MIN_ZOOM: 8,
         GLOBAL_MAP_DEFAULT_MAX_ZOOM: 16,
-        GLOBAL_MAP_ATTRIBUTION: '<a href="http://www.makina-corpus.com" target="_blank">Makina Corpus</a> | &copy; Contributeurs <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a>',
+        GLOBAL_MAP_ATTRIBUTION: '(c) IGN Geoportail',
     };
 
     /* Variables for filesystem tree on device
