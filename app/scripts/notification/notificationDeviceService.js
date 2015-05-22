2cce32b0d4a9e57ca4b207f290931c427b31a4b1
jma <jma@makina-corpus.com>
2015-05-22 12:20:46 +0200

    Aggregate notifications

--- app/scripts/notification/notificationDeviceService.js
+++ app/scripts/notification/notificationDeviceService.js
@@ -3,12 +3,13 @@
 var geotrekNotification = angular.module('geotrekNotification');
 
 geotrekNotification.service('notificationDeviceService', ['$q', '$timeout', function ($q, $timeout) {
-
+    var id = 0;
     this.notify = function(msg, title) {
-        window.plugin.notification.local.add({
-            id:      1,
+        id = id + 1;
+        window.plugin.notification.local.schedule({
+            id:      id,
             title:   title || 'Title',
-            message: msg
+            message:    msg,
         });
     };
 
