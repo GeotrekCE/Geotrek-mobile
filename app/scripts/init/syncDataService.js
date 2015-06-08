bbf4c4f09d5a7a7a5b92000123d89de0eaa6c09f
ebrehault <ebrehault@gmail.com>
2015-06-08 13:47:23 +0200

    app should wait for connexion instead of exit - invalid for apple

--- app/scripts/init/syncDataService.js
+++ app/scripts/init/syncDataService.js
@@ -10,37 +10,62 @@ geotrekInit.service('syncDataService', ['$q', '$window', '$cordovaDialogs', '$co
 
             var deferred = $q.defer();
             if (angular.isDefined($window.cordova)){
-
                 if ($cordovaNetwork.isOnline()) {
-
                     utils.downloadAndUnzip(globalizationSettings.FULL_DATA_REMOTE_FILE_URL, settings.device.CDV_ROOT + "/" + settings.device.RELATIVE_ROOT, false, progress('data'))
-                    .then(function(response) {
-                        if(!response.useCache) {
-                            return treksFactory.replaceImgURLs();
-                        }
-                    })
-                    .then(function() {
-                        return mapFactory.downloadGlobalBackground(settings.remote.MAP_GLOBAL_BACKGROUND_REMOTE_FILE_URL, progress('map'));
-                    })
-                    .then(function() {
-                        deferred.resolve();
-                    })
-                    .catch(function(error) {
-                        logging.warn(error);
-                        deferred.resolve(error);
-                    });
-                }
-                else {
-                    utils.isFirstTime().then(function(value){
-                        // if app is started for its firsttime and there is no network available.
-                        if (value === true) {
-                            $translate('init.error_first_loading').then(function(msg) {
-                                $cordovaDialogs.alert(msg, 'Info', 'OK').then(navigator.app.exitApp);
-                            });
-                        }
-                        // use cache
-                        else {
+                        .then(function(response) {
+                            if(!response.useCache) {
+                                return treksFactory.replaceImgURLs();
+                            }
+                        })
+                        .then(function() {
+                            console.log('download global zip');
+                            return mapFactory.downloadGlobalBackground(settings.remote.MAP_GLOBAL_BACKGROUND_REMOTE_FILE_URL, progress('map'));
+                        })
+                        .then(function() {
+                            console.log('resolve !');
                             deferred.resolve();
+                        })
+                        .catch(function(error) {
+                            logging.warn(error);
+                            deferred.resolve(error);
+                        });
+
+                } else {
+                    utils.isFirstTime().then(function(firstLaunch){
+                        if (!firstLaunch) {
+                            // use cache
+                            deferred.resolve();
+                        } else {
+                            $translate('init.error_first_loading').then(function(msg) {
+                                $cordovaDialogs.alert(msg, 'Info', 'OK')
+                                    .then(
+                                        function () {
+                                            console.log('Wait for Netwotk');
+                                            document.addEventListener('online', function () {
+                                                console.log('Netwotk found !');
+                                                utils.downloadAndUnzip(globalizationSettings.FULL_DATA_REMOTE_FILE_URL, settings.device.CDV_ROOT + "/" + settings.device.RELATIVE_ROOT, false, progress('data'))
+                                                    .then(function(response) {
+                                                        if(!response.useCache) {
+                                                            return treksFactory.replaceImgURLs();
+                                                        }
+                                                    })
+                                                    .then(function() {
+                                                        console.log('download global zip');
+                                                        return mapFactory.downloadGlobalBackground(settings.remote.MAP_GLOBAL_BACKGROUND_REMOTE_FILE_URL, progress('map'));
+                                                    })
+                                                    .then(function() {
+                                                        console.log('resolve !');
+                                                        deferred.resolve();
+                                                    })
+                                                    .catch(function(error) {
+                                                        logging.warn(error);
+                                                        deferred.resolve(error);
+                                                    });
+                                            }, false);
+                                        }
+                                    );
+                            });
+                            
                         }
                     })
                 }
