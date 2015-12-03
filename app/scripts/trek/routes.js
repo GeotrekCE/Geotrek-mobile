3c3c5c031f3b36906ee2b964bb1cb968073d5c60
Bastien Alvez <bastien.alvez@gmail.com>
2015-12-03 09:23:37 +0100

    Add one boolean setting TOURISTIC to ignore touristic content

--- app/scripts/trek/routes.js
+++ app/scripts/trek/routes.js
@@ -5,50 +5,50 @@ var geotrekTreks = angular.module('geotrekTreks', ['pascalprecht.translate']);
 geotrekTreks.config(function($stateProvider) {
 
     $stateProvider
-    .state('home', {
-        url: '',
-        abstract: true,
-        templateUrl: 'views/nav_trek_map.html',
-        controller: 'TrekController',
-        resolve: {
-            treks: function(treksFactory) {
-                return treksFactory.getTreks();
-            },
-            staticPages: function(staticPagesFactory) {
-                return staticPagesFactory.getStaticPages();
+        .state('home', {
+            url: '',
+            abstract: true,
+            templateUrl: 'views/nav_trek_map.html',
+            controller: 'TrekController',
+            resolve: {
+                treks: function(treksFactory) {
+                    return treksFactory.getTreks();
+                },
+                staticPages: function(staticPagesFactory) {
+                    return staticPagesFactory.getStaticPages();
+                }
             }
-        }
-    })
-    .state('home.trek', {
-        url: '/trek',
-        templateUrl : 'views/trek_list.html',
-        controller: 'TrekListController'
-    })
-    .state('home.trek.detail', {
-        url: '/trek-:trekId?parentId',
-        controller: 'TrekDetailController',
-        resolve: {
-            trek: function(treksFactory, $stateParams) {
-                return treksFactory.getTrek($stateParams.trekId);
-            },
-            pois: function(poisFactory, $stateParams) {
-                return poisFactory.getPoisFromTrek($stateParams.trekId);
-            },
-            touristics: function(touristicsFactory, $stateParams) {
-                return touristicsFactory.getAllTouristicsContentsFromATrek($stateParams.trekId);
-            },
-            downloadedTrek: function($q, trek, mapFactory) {
-                var promises = [];
-                promises.push(mapFactory.hasTrekPreciseBackground(trek.id));
-                return $q.all(promises)
-                .then(function(isDownloadedList) {
-                    for(var i=0; i<isDownloadedList.length; i++) {
-                        trek['tiles'] = {
-                            isDownloaded: isDownloadedList[i]
-                        };
-                    }
-                });
+        })
+        .state('home.trek', {
+            url: '/trek',
+            templateUrl: 'views/trek_list.html',
+            controller: 'TrekListController'
+        })
+        .state('home.trek.detail', {
+            url: '/trek-:trekId?parentId',
+            controller: 'TrekDetailController',
+            resolve: {
+                trek: function(treksFactory, $stateParams) {
+                    return treksFactory.getTrek($stateParams.trekId);
+                },
+                pois: function(poisFactory, $stateParams) {
+                    return poisFactory.getPoisFromTrek($stateParams.trekId);
+                },
+                touristics: function(touristicsFactory, settings, $stateParams) {
+                    return (settings.TOURISTIC ? touristicsFactory.getAllTouristicsContentsFromATrek($stateParams.trekId) : []);
+                },
+                downloadedTrek: function($q, trek, mapFactory) {
+                    var promises = [];
+                    promises.push(mapFactory.hasTrekPreciseBackground(trek.id));
+                    return $q.all(promises)
+                        .then(function(isDownloadedList) {
+                            for (var i = 0; i < isDownloadedList.length; i++) {
+                                trek['tiles'] = {
+                                    isDownloaded: isDownloadedList[i]
+                                };
+                            }
+                        });
+                }
             }
-        }
-    });
+        });
 });
