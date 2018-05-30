bff283580c3f4cfb2d93fd143935ec812b6116ae
Bastien <bastien.alvez@gmail.com>
2018-05-30 14:07:55 +0200

    update nofitication service

--- app/scripts/notification/notificationDeviceService.js
+++ app/scripts/notification/notificationDeviceService.js
@@ -6,10 +6,11 @@ geotrekNotification.service('notificationDeviceService', ['$q', '$timeout', func
     var id = 0;
     this.notify = function(msg, title) {
         id = id + 1;
-        window.plugin.notification.local.schedule({
+        cordova.plugins.notification.local.schedule({
             id:      id,
             title:   title || 'Title',
             text:    msg,
+            foreground: true
         });
     };
 
