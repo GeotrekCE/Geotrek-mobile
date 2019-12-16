385e03a146f96738cc244643481da9bf1bdef326
bastyen <bal@makina-corpus.com>
2019-12-16 15:26:52 +0100

    add items test

---
+++ cypress/integration/more.spec.js
@@ -0,0 +1,16 @@
+describe('More', () => {
+  it('should go to more tab', () => {
+    cy.visit('/app/tabs/more');
+  });
+
+  it('should got items', () => {
+    cy.get('.list-md')
+      .children()
+      .should('have.length.greaterThan', 0);
+  });
+
+  it('should go to one item', () => {
+    cy.get('.list-md > :nth-child(1)').click({ force: true });
+    cy.url().should('include', '/app/tabs/more/');
+  });
+});
